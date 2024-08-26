0984c6577b2d0d80d651419d2b9ab154945e5323
LobstyuII <2001212684@alumni.pku.edu.cn>
2024-08-26 21:17:46 +0800

    Latest Release 20240826

--- celery_app.py
+++ celery_app.py
@@ -11,5 +11,5 @@ app.conf.update(
     enable_utc=True,
     worker_log_format='[%(asctime)s: %(levelname)s/%(processName)s] %(message)s',
     worker_task_log_format='[%(asctime)s: %(levelname)s/%(processName)s] %(task_name)s[%(task_id)s]: %(message)s',
-    worker_redirect_stdouts_level='INFO',  # 确保 INFO 级别的输出被显示
+    worker_redirect_stdouts_level='INFO',
 )
